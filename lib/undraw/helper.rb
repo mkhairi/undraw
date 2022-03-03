0e30b58090f7c848e179c5dc3714ca2a9c85d7d4
Mohd Khairi Mohd Adnan <khairi@labs.my>
2022-03-04 07:45:05 +0800

    Rename method

--- lib/undraw/helper.rb
+++ lib/undraw/helper.rb
@@ -1,9 +1,9 @@
 module Undraw
   module Helper
     def undraw(filename, options = {})
-      svg_file = read_svg(filename)
+      svg_file = read_undraw_svg(filename)
       color = options.delete(:color)
-      output = recolor(svg_file, color)
+      output = recolor_undraw_svg(svg_file, color)
       InlineSvg::TransformPipeline.generate_html_from(output, options).html_safe
     rescue Errno::ENOENT => e
       raise Undraw::FileNotFound,
@@ -12,13 +12,13 @@ module Undraw
 
     private
 
-    def read_svg(filename)
+    def read_undraw_svg(filename)
       asset_path = "#{Undraw.root}/vendor/assets/images/undraw/#{filename}.svg"
       vendor_path = "#{Rails.root}/vendor/assets/images/undraw/#{filename}.svg"
       File.read(File.exist?(vendor_path) ? vendor_path : asset_path)
     end
 
-    def recolor(svg_file, color)
+    def recolor_undraw_svg(svg_file, color)
       if color.is_a?(Hash)
         # :todo:
         # = undraw("building", color: { primary: #ff6347, hair: black }, class: 'undraw-features', size: "350*150")
